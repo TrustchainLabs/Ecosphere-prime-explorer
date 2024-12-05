e013eb948dea19f4ec6002608f0eefa406c63657
scarecrow-11 <hafizurmilon.11@gmail.com>
2024-12-05 16:54:20 +0800

    [bug] Remove custom map

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,18 +1,49 @@
-import { CustomMap } from '../common/custom-map'
+'use client'
+import { useRef, useState, useEffect } from 'react'
+import DeckGL from '@deck.gl/react'
+import { Map as BaseMap } from 'react-map-gl/maplibre'
+import styles from './Map.module.css'
 
 export const Map = () => {
+    const mapContainerRef: any = useRef(null)
+    const [mapSize, setMapSize] = useState({ width: 600, height: 600 })
+
+    useEffect(() => {
+        handleResize()
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    const handleResize = () => {
+        if (mapContainerRef.current) {
+            setMapSize(prevMapSize => ({
+                ...prevMapSize,
+                width: mapContainerRef.current.offsetWidth,
+                height: mapContainerRef.current.offsetHeight
+            }))
+        }
+    }
+
     return (
-        <CustomMap
-            initialViewState={{
-                longitude: 101.70977605792783,
-                latitude: 3.157292964783922,
-                zoom: 6
-            }}
-            minMapSize={{
-                width: 600,
-                height: 600
-            }}
-            mapStyle='https://demotiles.maplibre.org/style.json'
-        />
+        <div ref={mapContainerRef} className={styles.container}>
+            <DeckGL
+                initialViewState={{
+                    longitude: 101.70977605792783,
+                    latitude: 3.157292964783922,
+                    zoom: 6
+                }}
+                controller={true}
+                width={mapSize.width}
+                height={mapSize.height}
+            >
+                <BaseMap
+                    reuseMaps={true}
+                    mapStyle='https://demotiles.maplibre.org/style.json'
+                />
+            </DeckGL>
+        </div>
     )
 }
