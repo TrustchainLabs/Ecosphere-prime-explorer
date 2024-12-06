ba1bebdd2ca2cce1bfab53febc78607640e9d501
scarecrow-11 <hafizurmilon.11@gmail.com>
2024-12-06 12:16:29 +0800

    [refactor] Replace maplibre and deck.gl with mapbox

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,49 +1,23 @@
 'use client'
-import { useRef, useState, useEffect } from 'react'
-import DeckGL from '@deck.gl/react'
-import { Map as BaseMap } from 'react-map-gl/maplibre'
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+import { MAPBOX_ACCESS_TOKEN } from '../../shared/constants'
 import styles from './Map.module.css'
 
 export const Map = () => {
-    const mapContainerRef: any = useRef(null)
-    const [mapSize, setMapSize] = useState({ width: 600, height: 600 })
+    const mapRef: any = useRef(null)
 
     useEffect(() => {
-        handleResize()
-        window.addEventListener('resize', handleResize)
-
-        return () => {
-            window.removeEventListener('resize', handleResize)
-        }
+        mapRef.current = new mapboxgl.Map({
+            container: 'map',
+            accessToken: MAPBOX_ACCESS_TOKEN,
+            style: 'mapbox://styles/mapbox/dark-v10',
+            center: [101.70977605792783, 3.157292964783922],
+            zoom: 6
+        })
     }, [])
 
-    const handleResize = () => {
-        if (mapContainerRef.current) {
-            setMapSize(prevMapSize => ({
-                ...prevMapSize,
-                width: mapContainerRef.current.offsetWidth,
-                height: mapContainerRef.current.offsetHeight
-            }))
-        }
-    }
-
     return (
-        <div ref={mapContainerRef} className={styles.container}>
-            <DeckGL
-                initialViewState={{
-                    longitude: 101.70977605792783,
-                    latitude: 3.157292964783922,
-                    zoom: 6
-                }}
-                controller={true}
-                width={mapSize.width}
-                height={mapSize.height}
-            >
-                <BaseMap
-                    reuseMaps={true}
-                    mapStyle='https://demotiles.maplibre.org/style.json'
-                />
-            </DeckGL>
-        </div>
+        <div id='map' className={styles.container} />
     )
 }
